e361d37d3213d16db43ff652f79128cba7833393
Vitaliy K <vitaliyx@gmail.com>
2024-04-11 19:25:02 +0300

    Switch case

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 
 
-let rezult = (2+2===4) ? 'Дійсно чотири' : 'Чогось не чотири';
-console.log(rezult);
+const userDrink = prompt('Виберіть напій: \n1 - Чай \n2- Кава \n3 - Сік \n4 - Вода \n5 - Лимонад');
+debugger;
+
+switch (userDrink) {
+    case '1':
+        console.log('Чай');
+        break;
+    case '2':
+        console.log('Кава');
+        break;
+    case '3':
+        console.log('Сік');
+        break;
+    case '4':
+        console.log('Вода');
+        break;
+    case '5':
+        console.log('Лимонад');
+        break;
+    default:
+        console.log('Неизвестно');
+}
