ad71c7c4950bcceb314f4ca3eebee89f535cbaf9
Vitaliy K <vitaliyx@gmail.com>
2024-04-11 20:21:08 +0300

    High ORDER Function

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 
 
-const userDrink = prompt('Виберіть напій: \n1 - Чай \n2- Кава \n3 - Сік \n4 - Вода \n5 - Лимонад');
-debugger;
-
-switch (userDrink) {
-    case '1':
-        console.log('Чай');
-        break;
-    case '2':
-        console.log('Кава');
-        break;
-    case '3':
-        console.log('Сік');
-        break;
-    case '4':
-        console.log('Вода');
-        break;
-    case '5':
-        console.log('Лимонад');
-        break;
-    default:
-        console.log('Неизвестно');
+function square(b) {
+    return b*b;
 }
+
+function superFunction(number, fn) { // функція вищого порядку (High Order Function - HOF)
+    
+    fn(number);  // callback - функція зворотного виклику
+
+}
+
+superFunction (5, square);
