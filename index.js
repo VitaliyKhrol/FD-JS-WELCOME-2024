728cdcc2995bafed0e29b6a4166b1099161b9fa1
Vitaliy K <vitaliyx@gmail.com>
2024-04-15 18:48:24 +0300

    fizzBuzz

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 
-let i=1;
-while (i<=10){
-    if (i%2===0) console.log(i);
-    i++;
+
+// function factorial(n) {
+//     // debugger;
+//     let factor = 1;
+//     for (let i = 1; i <= n; i++) {
+//         factor = factor * i;
+//     }
+//     console.log(factor)
+// }
+
+// factorial(5);
+
+
+// function pow(bace, power) {
+//     // debugger;
+//     let j = bace;
+//     for (let i = 1; i < power; i++) {
+//         j = j * bace;
+//     }
+//     console.log(j)
+// }
+
+// pow(5, 3)
+
+// function square(a, b) {
+//     return a * b;
+// }
+
+
+
+function fizzBuzz(number) {
+    for (let i = 1; i <= number; i++) {
+        if ((i % 5 === 0) && (i % 3 === 0)) {
+            console.log('FizzBuzz')
+        }
+        else if (i % 5 === 0) {
+            console.log('Buzz')
+        }
+        else if (i % 3 === 0) {
+            console.log('Fizz')
+        }
+        else (
+            console.log(i)
+        )
+    }
 }
+
+
+fizzBuzz(100)
