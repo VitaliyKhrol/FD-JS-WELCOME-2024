dc19fc50d6f7a1892e85f46c4ecc0567d2fd91e0
Vitaliy K <vitaliyx@gmail.com>
2024-04-16 19:25:41 +0300

    Task resolve

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 
-let cat = {
-    breed: 'Blue Cheshire',
-    name: 'Murzik',
-    age: 3,
-    weight: 10,
-    color: 'grey',
-    sleep: function () {
-        console.log('I am sleping')
-    },
-    eat: function () {
-        console.log('I am eating')
-    }
+// let cat = {
+//     breed: 'Blue Cheshire',
+//     name: 'Murzik',
+//     age: 3,
+//     weight: 10,
+//     color: 'grey',
+//     sleep: function () {
+//         console.log('I am sleping')
+//     },
+//     eat: function () {
+//         console.log('I am eating')
+//     },
+//     2: 'NewValue'
+// }
+
+
+// let murka ={
+//     breed: 'British',
+//     name: 'Murka',
+//     age: 2,
+//     weight: 5,
+//     color: 'red',
+//     sleep: function () {
+//         console.log('I am sleping')
+//     },
+//     eat: function () {
+//         console.log('I am eating')
+//     }
+// }
+
+// cat.girlfriend =murka;
+
+
+const weekPlan = {
+    Mon: 'dantist',
+    Tue: 'go to park',
+    Wed: 'meet friends',
+    Thu: 'learn',
+    Fri: 'hard work',
+    Sat: 'go to party'
 }
 
-// Доступ
+function getWeekPlan(plan) {
+    const userValue = prompt('Який день неділі тебе цікавить?')
+    alert(plan[userValue])
 
-cat.breed; // властивості
-cat.eat();  // методи
+}
 
-// Зміна
-
-cat.breed = 'Dvorovaja'
-
-// Видалення
-
-delete cat.breed;
-delete cat.sleep
-
+getWeekPlan(weekPlan)
