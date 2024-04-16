27d1273c2dfebb4220c4c98d9ef25c8c83e0b64d
Vitaliy K <vitaliyx@gmail.com>
2024-04-16 09:40:43 +0300

    Object welcome

--- index.js
+++ index.js
@@ -1,50 +1,29 @@
 
-
-// function factorial(n) {
-//     // debugger;
-//     let factor = 1;
-//     for (let i = 1; i <= n; i++) {
-//         factor = factor * i;
-//     }
-//     console.log(factor)
-// }
-
-// factorial(5);
-
-
-// function pow(bace, power) {
-//     // debugger;
-//     let j = bace;
-//     for (let i = 1; i < power; i++) {
-//         j = j * bace;
-//     }
-//     console.log(j)
-// }
-
-// pow(5, 3)
-
-// function square(a, b) {
-//     return a * b;
-// }
-
-
-
-function fizzBuzz(number) {
-    for (let i = 1; i <= number; i++) {
-        if ((i % 5 === 0) && (i % 3 === 0)) {
-            console.log('FizzBuzz')
-        }
-        else if (i % 5 === 0) {
-            console.log('Buzz')
-        }
-        else if (i % 3 === 0) {
-            console.log('Fizz')
-        }
-        else (
-            console.log(i)
-        )
+let cat = {
+    breed: 'Blue Cheshire',
+    name: 'Murzik',
+    age: 3,
+    weight: 10,
+    color: 'grey',
+    sleep: function () {
+        console.log('I am sleping')
+    },
+    eat: function () {
+        console.log('I am eating')
     }
 }
 
+// Доступ
 
-fizzBuzz(100)
+cat.breed; // властивості
+cat.eat();  // методи
+
+// Зміна
+
+cat.breed = 'Dvorovaja'
+
+// Видалення
+
+delete cat.breed;
+delete cat.sleep
+
