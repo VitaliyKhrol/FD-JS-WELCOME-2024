419b5c515f2f345ebdf38a7927ab5f4a792c4983
Vitaliy K <vitaliyx@gmail.com>
2024-04-20 12:37:39 +0300

    sumOfAllArguments

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
+'use strict'
+
+function sumOfAllArguments() {
+    let sum = 0;
+    console.log(arguments)
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+    }
+    return sum;
+}
+
 
