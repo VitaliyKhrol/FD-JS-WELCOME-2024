bf8a76f905e5ede25565e249cd4c19daaeba04e2
Vitaliy K <vitaliyx@gmail.com>
2024-04-21 19:27:14 +0300

    sort

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 
 'use strict'
 
-function Ladder(){
-    this.carrentStep = 0;
+function User(firstName, lastName, age, mail, isSubscribe = false) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.mail = mail;
+    this.isSubscribe = isSubscribe;
+
 }
 
 
-function LadderProto(){
-    
-    this.up = function up(){
-        debugger;
-        ++this.carrentStep;
-        return this;
-    }
-    this.down = function down(){
-       --this.carrentStep;
-       return this;
-    }
-
-    this.showStep = function showStep(){
-        return this.carrentStep;
-    }
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 
-Ladder.prototype = new LadderProto();
+function createArrayOfUsers(quantity) {
+    const userArr = [];
+    for (let i = 0; i < quantity; i++) {
+        userArr.push(new User(`FirstName ${i}`, `LastName ${i}`, Math.ceil(getRandomArbitrary(10, 95)), `mail${i}@gmail.com`, Boolean(Math.round(Math.random()))))
+    }
+
+    return userArr;
+}
+
+const arrUsers = createArrayOfUsers(50);
+// console.table(arrUsers);
+
+
+const fNameArray = arrUsers.map(function (userObj) { return `${userObj.firstName} ${userObj.lastName}` });
+
+arrUsers.sort(function (usA,usB){
+  return usA.age-usB.age;
+});
+
+console.table (arrUsers)
