80685c72a43c156d1f4a7249464b87e2bfc3eb06
Vitaliy K <vitaliyx@gmail.com>
2024-04-23 09:56:24 +0300

    Double Loop

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
 
 'use strict'
 
-function sum (a,b, ...array) {
-    console.log(a);
-    console.log(b);
-    console.log(array);
-
+for (let i = 1; i < 10; i++) {
+    for (let j = 1; j < 10; j++) {
+    console.log (`${i} * ${j} = ${i*j}`)
+    }
+    console.log('----------------')
 }
-
-// sum(1,2,3,4,5,6,7,8,9)
-
-const sumOfArguments = (...array)=>
-    array.reduce((accumulator, currentValue)=> accumulator + currentValue
-    ,0 );
-
-
-
-// console.log (sumOfArguments(1,2,3,4,5,6,7,8))
-
-
-function sum1(a,b,c){
-    return a+b+c;
-}
-
-const numbers =[1,2,3,4,5,-1]
-
-
-const numbers1 = [...numbers];
