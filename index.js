6207dd9bd2ae42d28bde6caf958e2db425575b19
Vitaliy K <vitaliyx@gmail.com>
2024-04-26 09:25:08 +0300

    Stec ilustration

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-
 'use strict'
 
-
-function drawSquare(dimension) {
-
-    let square = '';
-    for (let i = 0; i <= dimension; i++) {
-        for (let j = 0; j <= dimension; j++) {
-            if (i === 0 || i === dimension || j === 0 || j === dimension || (dimension-i) === j) {
-                square += '*';
-            } else { square += ' ' }
-        }
-        square += '\n'
-    }
-    console.log(square)
+function sum(a,b) {
+    return (a+b);
+    
 }
 
-drawSquare(20)
+
+function multy(a,b){
+    return a*b;
+}
+
+function consoleResult (){
+    console.log (sum(2,(multy(2,2))))
+}
+
+consoleResult ();
