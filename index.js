5c83c22225ad0eadbf980162acd6bea28b85eacf
Vitaliy K <vitaliyx@gmail.com>
2024-04-26 12:41:52 +0300

    SumOfArrays

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 'use strict'
 
-function sum(a,b) {
-    return (a+b);
-    
+
+
+function factorial(number) {
+    // debugger;
+    if (number === 1) {
+        return 1;
+    }
+    return number * factorial(number - 1);
+
+}
+
+factorial(5);
+
+
+function pow(base, power) {
+    if (power === 1) {
+        return base;
+    }
+    return base * pow(base, power - 1)
 }
 
 
-function multy(a,b){
-    return a*b;
-}
+const pow2 = (base, power) => (power === 1) ? base : base * pow2(base, power - 1);
 
-function consoleResult (){
-    console.log (sum(2,(multy(2,2))))
-}
+const array1 =[1,2,3,4,5]
 
-consoleResult ();
+const array = [2, 3, 2, 1, [4, 2, 3, [6, 3, 2, [3], 3, 3], 2]]
+
+function sumMasiv(array) {
+    // debugger;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (!Array.isArray(array[i]) ) {
+            sum += array[i];
+        } else sum += sumMasiv (array[i])
+    }
+    return sum;
+}
