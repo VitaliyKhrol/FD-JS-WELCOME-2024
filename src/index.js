cfbd977fda0552430da493fd4839f6c62c30060b
Vitaliy K <vitaliyx@gmail.com>
2024-05-12 17:09:45 +0300

    BinarySearch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 'use strict'
 
 
+const arr = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function binarySearch(array, searchValue) {
+    let start = 0;
+    let end = array.length - 1;
+    let middle = Math.round((start + end) / 2);
+
+    while (true) {
+        if (searchValue === array[middle]) {
+            return (middle)
+        }
+        if (searchValue > array[middle]) {
+            start = middle;
+            middle = Math.ceil((start + end) / 2);
+        } else {
+            end = middle;
+            middle = Math.floor((start + end) / 2);
+        }
+    }
+}
