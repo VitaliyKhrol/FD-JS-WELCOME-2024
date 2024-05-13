7c8595054e8da28cd25af573e5a9f3e005b266a8
Vitaliy K <vitaliyx@gmail.com>
2024-05-13 12:25:52 +0300

    Stack

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 'use strict'
 
-
-const arr = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-function binarySearch(array, searchValue) {
-    let start = 0;
-    let end = array.length - 1;
-    let middle = Math.round((start + end) / 2);
-
-    while (true) {
-        if (searchValue === array[middle]) {
-            return (middle)
+class Stack {
+    constructor(maxSize, ...arr) {
+        this._maxSize = maxSize;
+        this._size = 0;
+        for (const i of arr) {
+            this.push(i)
         }
-        if (searchValue > array[middle]) {
-            start = middle;
-            middle = Math.ceil((start + end) / 2);
+    }
+
+    get size() {
+        return this._size;
+    }
+
+    get isEmpty() {
+        return this._size === 0;
+    }
+
+    push(value) {
+ 
+        if (this._size >= this._maxSize) {
+            throw new RangeError('Stack overflow')
         } else {
-            end = middle;
-            middle = Math.floor((start + end) / 2);
+            this[`_${this.size}`] = value;
+            this._size++;
+            return this._size;
         }
+    }
+
+    pop() {
+        const lastItem = this[`_${this.size - 1}`];
+        delete this[`_${this.size - 1}`];
+        this._size--;
+        return lastItem;
+
+    }
+
+    pick (){
+        return this[`_${this.size-1}`]
     }
 }
