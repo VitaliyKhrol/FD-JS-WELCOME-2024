dfa6e9978a4574eabd629768b3de8d737cee1e6e
Vitaliy K <vitaliyx@gmail.com>
2024-05-14 18:11:22 +0300

    destructurization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 
 
-const arr1 =[2,7,5,1,9,-10];
-const arr2 = [4,2,7,10]
+const arr1 = [2, 7, 5, 1, 9, -10];
+const arr2 = [4, 2, 7, 10]
 
 const arrNew = [...(new Set(arr1.concat(arr2)))]
+
+
+
+const map = new Map([[1, { username: 'john Doe' }], [2, { username: 'jane Doe' }], [3, { username: 'alex Doe' }]]);
+
+const res = [...map.entries()].map((cortage) => { return `${cortage[0]} ${cortage[1].username}` })
+
+
+
+const userObj = {
+    name: {
+        first: 'John',
+        last: 'Doe',
+    },    
+    age: 20,
+    aut:{
+       email: 'iii@gmail.com',
+       pass: 'qwerty' 
+    }
+}
+
+const {age}= userObj;
+const {name: {first,last}} = userObj;
+const {aut: {email,pass}} = userObj;
