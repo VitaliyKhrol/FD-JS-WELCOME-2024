402d3a097de18d6ba1ee0774bb7efea0b6c40a0a
Vitaliy K <vitaliyx@gmail.com>
2024-05-14 09:11:48 +0300

    mergeQueues task resolve

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+
+
+
 class Queue {
-    constructor(){
+    constructor(...arr){
         this._head =0;
         this._tail=0;
+
+        for (const i of arr) {
+            this.enqueue(i);
+        }
     }
 
     get size(){
@@ -27,6 +34,20 @@ class Queue {
             return firstItem;
         }
     }
+}
+
+
+const q1 = new Queue(1,3,5,7)
+const q2 = new Queue (2,4,6,8,10,12)
+
+function mergeQueues(q1,q2){
+    const qq= new Queue();
+    while(q1.size || q2.size){
+        qq.enqueue(q1.dequeue());
+        qq.enqueue(q2.dequeue());
+    }
+    
+    return qq;
 }
 
 
